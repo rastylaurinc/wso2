6183cad4455a33823c69a358e58db0c30ebd2db8
Rasty Laurinc <rasty.laurinc@gmail.com>
2020-07-15 03:00:53 +0800

    deployment-scripts/set-deployment-context.sh: read Jenkins variables

--- deployment-scripts/set-deployment-context.sh
+++ deployment-scripts/set-deployment-context.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #log start
 echo 'set-deployment-context.sh: started'
+echo 'set-deployment-context.sh: docker-organization = '$DOCKER_ORG
+echo 'set-deployment-context.sh: docker-repository = '$DOCKER_REPOSITORY
 
 #log successfull finish
 echo 'set-deployment-context.sh: completed successfully'
